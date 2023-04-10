8a5a38a8ba7d9fd5a780010a76a853222c38f35b
dozee <yuriy.dozer@gmail.com>
2023-04-10 18:38:55 +0300

    reorganize

--- variable.tf
+++ variable.tf
@@ -31,6 +31,33 @@ variable "my_ip" {
   default = "176.117.187.49/32"
 }
 
+#____________________________________________MAIN_KEY_VARIABLE
+# for deploy code make key value empty and set variable on terraform cloud
+variable "external_key_name" {
+  default = "external_key_pub"
+}
+variable "external_key" {
+  default = "-----BEGIN RSA PRIVATE KEY-----MIIBigKCAYEAq3DnhgYW7yWGjXPqy/ZJ/+-----END RSA PRIVATE KEY-----"
+  #default = file("/home/dozee/.ssh/external_key.pub")
+}
+variable "private_internal_key" {
+  default = "-----BEGIN RSA PRIVATE KEY-----MIIBigKCAYEAq3DnhgYW7yWGjXPqy/ZJ/+-----END RSA PUBLIC KEY-----"
+  #default = file("/home/dozee/.ssh/internal_key")
+}
+variable "destination_private_internal_key" {
+  default = "/home/ec2-user/.ssh/private_internal_key"
+}
+variable "destination_private_internal_key_ubuntu" {
+  default = "/home/ubuntu/private_internal_key.pub"
+}
+variable "internal_key_name" {
+  default = "internal_key_pub"
+}
+variable "internal_key" {
+  default = "-----BEGIN RSA PRIVATE KEY-----MIIBigKCAYEAq3DnhgYW7yWGjXPqy/ZJ/+-----END RSA PUBLIC KEY-----"
+  #default = file("/home/dozee/.ssh/internal_key.pub")
+}
+
 #____________________________________________MAIN_RDS_VARIABLE
 variable "create_db_instance" {
   default = true
@@ -84,7 +111,6 @@ variable "username" {
 variable "db_password" {
   default = "12345678"
 }
-
 
 #____________________________________________MAIN_BASTION_VARIABLE
 variable "bastion_count" {
@@ -144,7 +170,8 @@ variable "public_zone_name" {
   description = "Public Hosted Zone Name"
   default     = "minmax.click"
 }
-#StateGitLab_registry
+
+#____________________________________________StateGitLab_registry
 variable "token_registry" {
   default = "glpat-sauQ_1iszVxgEoq1s-W-"  #for read registry
 }
@@ -181,33 +208,6 @@ variable "elk_user_ubuntu" {
 }
 variable "elk_user_awslinux" {
   default = "ec2-user"
-}
-
-#____________________________________________MAIN_KEY_VARIABLE
-# for deploy code make key value empty and set variable on terraform cloud
-variable "external_key_name" {
-  default = "external_key_pub"
-}
-variable "external_key" {
-  default = "-----BEGIN RSA PUBLIC KEY-----MIIBigKCAYEAq3DnhgYgLVJknvDA3clATozPtjI7yauqD4/ZuqgZn4KzzzkQ4BzJar4jRygpzbghlFn0Luk1mdVKzPUgYj0VkbRlHyYfcahbgOHixOOnXkKXrtZW7yWGjXPqy/ZJ/+-----END RSA PUBLIC KEY-----"
-  #default = file("/home/dozee/.ssh/external_key.pub")
-}
-variable "private_internal_key" {
-  default = "-----BEGIN RSA PRIVATE KEY-----MIIBigKCAYEAq3DnhgYgLVJknvDA3clATozPtjI7yauqD4/ZuqgZn4KzzzkQ4BzJar4jRygpzbghlFn0Luk1mdVKzPUgYj0VkbRlHyYfcahbgOHixOOnXkKXrtZW7yWGjXPqy/ZJ/+-----END RSA PUBLIC KEY-----"
-  #default = file("/home/dozee/.ssh/internal_key")
-}
-variable "destination_private_internal_key" {
-  default = "/home/ec2-user/.ssh/private_internal_key"
-}
-variable "destination_private_internal_key_ubuntu" {
-  default = "/home/ubuntu/private_internal_key.pub"
-}
-variable "internal_key_name" {
-  default = "internal_key_pub"
-}
-variable "internal_key" {
-  default = "-----BEGIN RSA PUBLIC KEY-----MIIBigKCAYEAq3DnhgYgLVJknvDA3clATozPtjI7yauqD4/ZuqgZn4KzzzkQ4BzJar4jRygpzbghlFn0Luk1mdVKzPUgYj0VkbRlHyYfcahbgOHixOOnXkKXrtZW7yWGjXPqy/ZJ/+-----END RSA PUBLIC KEY-----"
-  #default = file("/home/dozee/.ssh/internal_key.pub")
 }
 
 #____________________________________________MONITOR
